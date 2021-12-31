bd209b97a982bbcece3ca3858df27aad8532e2b2
viki-119 <1192756137@qq.com>
2021-12-31 16:29:46 +0800

    delete

--- test/index.js
+++ test/index.js
@@ -3,7 +3,6 @@ import ReactDom from 'react-dom';
 import { hot } from 'react-hot-loader/root';
 import Empty from '@/components/Empty';
 
-const zzdd = 'asdf';
 class App extends PureComponent {
   render() {
     return (
